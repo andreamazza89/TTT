aca21bfed65cb97298aec26f7c5b1949d0e1880a
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-10-22 23:49:33 +0100

    Add draw_board example to BoardPrinter

--- spec/unit/board_printer_spec.rb
+++ spec/unit/board_printer_spec.rb
@@ -1,10 +1,19 @@
-describe BoardPrinter do
+describe BoardPrinter, '#stringify_board' do
 
   it 'prints the board (empty example)' do
-    mock_board = instance_double('Board', board_state: [[nil,nil,nil], [nil,nil,nil], [nil,nil,nil]])
-    visualised_board = described_class.stringify_board(mock_board)
+    empty_board = [[nil,nil,nil], [nil,nil,nil], [nil,nil,nil]]
+    mock_board = instance_double('Board', board_state: empty_board)
 
+    visualised_board = described_class.stringify_board(mock_board)
     expect(visualised_board).to eq VISUAL_BOARD[:no_moves]
+  end
+
+  it 'prints the board (draw example)' do
+    draw_board = [['x','o','x'], ['x','o','o'], ['o','x','x']]
+    mock_board = instance_double('Board', board_state: draw_board)
+
+    visualised_board = described_class.stringify_board(mock_board)
+    expect(visualised_board).to eq VISUAL_BOARD[:draw]
   end
 
 end
