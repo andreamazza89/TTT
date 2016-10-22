52cdfd9e7227375d34c8d197ef4735b233d26c85
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-10-22 23:39:40 +0100

    Add test for initialised board

--- spec/unit/board_spec.rb
+++ spec/unit/board_spec.rb
@@ -1,3 +1,13 @@
+describe Board, '#board_state' do
+
+  it 'returns an empty board before any moves are added' do
+    board = described_class.new
+
+    expect(board.board_state).to eq [[nil, nil, nil], [nil, nil, nil], [nil, nil, nil]]
+  end
+
+end
+
 describe Board, '#add_move' do
 
   it 'updates the board state with the cell/flag provided' do
