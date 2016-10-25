90525ea74f9d2ee2a288b8c31c224761b15d1481
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-10-25 08:16:37 +0100

    Update BoardPrinter to use new Cell class

--- lib/board_printer.rb
+++ lib/board_printer.rb
@@ -23,20 +23,20 @@ class BoardPrinter
   end
 
   def board_rows
-    board.board_state.map.with_index do |row_content, row_index| 
+    board.rows.map.with_index do |row_cells, row_index| 
       GAME_PROMPTS[:board_horizontal_spacer] + 
-      stringified_row(row_index)
+      stringify_row(row_index)
     end
   end
 
-  def stringified_row(row_number)
+  def stringify_row(row_number)
     stringified_row = NUMBER_TO_LETTER[row_number] + 
                       stringify_row_cells(row_number) + "\n"
   end
 
   def stringify_row_cells(row_number)
-    board.board_state[row_number].map do |cell|
-      cell.nil? ? '   ' : " #{cell} "
+    board.rows[row_number].map do |cell|
+      cell.flag.nil? ? '   ' : " #{cell.flag} "
     end.join('|')
   end
 
--- spec/unit/board_printer_spec.rb
+++ spec/unit/board_printer_spec.rb
@@ -1,18 +1,25 @@
 describe BoardPrinter, '#stringify_board' do
 
   it 'prints the board (empty example)' do
-    empty_board = [[nil,nil,nil], [nil,nil,nil], [nil,nil,nil]]
-    mock_board = instance_double('Board', board_state: empty_board)
+    empty_board = Board.new
 
-    visualised_board = described_class.stringify_board(mock_board)
+    visualised_board = described_class.stringify_board(empty_board)
     expect(visualised_board).to eq VISUAL_BOARD[:no_moves]
   end
 
   it 'prints the board (draw example)' do
-    draw_board = [['x','o','x'], ['x','o','o'], ['o','x','x']]
-    mock_board = instance_double('Board', board_state: draw_board)
+    draw_board = Board.new
+    draw_board.add_move('A1', 'x')
+    draw_board.add_move('A2', 'o')
+    draw_board.add_move('A3', 'x')
+    draw_board.add_move('B2', 'o')
+    draw_board.add_move('B1', 'x')
+    draw_board.add_move('B3', 'o')
+    draw_board.add_move('C2', 'x')
+    draw_board.add_move('C1', 'o')
+    draw_board.add_move('C3', 'x')
 
-    visualised_board = described_class.stringify_board(mock_board)
+    visualised_board = described_class.stringify_board(draw_board)
     expect(visualised_board).to eq VISUAL_BOARD[:draw]
   end
 
