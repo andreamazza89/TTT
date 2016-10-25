ccadff9cc4ea9db704f6ace11c202e6636df867f
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-10-25 13:14:17 +0100

    Implement logic to get AI to choose winning move in row when available

--- spec/unit/cpu_player_spec.rb
+++ spec/unit/cpu_player_spec.rb
@@ -38,13 +38,6 @@ describe CpuPlayer, '#next_move' do
       expect(cpu_one.next_move(board_state)).to eq "B3"
     end
 
-    it 'returns the winning move (column example 1)' do
-      cpu_one = described_class.new(flag: 'x')
-      board_state = [[nil, nil, 'o'], ['x', 'o', nil], ['x', nil, nil]]
-
-      expect(cpu_one.next_move(board_state)).to eq "A1"
-    end
-
   end
 
 end
