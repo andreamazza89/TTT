b57f44f5e82a7d79dd3f68ae5de19f877cd797d0
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-11-08 15:33:09 +0000

    Update game runner to instantiate GameSettings with GameEngine

--- console_game_runner.rb
+++ console_game_runner.rb
@@ -6,9 +6,9 @@ require_relative './lib/user_interface'
 ui = UserInterface.new(input: STDIN, output: STDOUT)
 ui.welcome
 
-game_settings = GameSettings.new(user_interface: ui)
+game = GameEngine.new(user_interface: ui)
+game_settings = GameSettings.new(user_interface: ui, game_engine: game)
 game_settings.select_game_mode
 game_settings.select_playing_order
 
-game = GameEngine.new(user_interface: ui, game_settings: game_settings)
 game.play
